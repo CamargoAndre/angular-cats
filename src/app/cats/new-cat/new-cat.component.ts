2497850c4a3e084d9ac982118c1e7706fec9c83a
Andre de Camargo <camargo_and@hotmail.com>
2023-07-14 09:14:41 -0300

    configurando do compmpnete new cat

--- src/app/cats/new-cat/new-cat.component.ts
+++ src/app/cats/new-cat/new-cat.component.ts
@@ -8,16 +8,26 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class NewCatComponent implements OnInit{
 
+  dataSource = [
+    {id:1, name: 'Cesar', length: 0.3, weight: 4.0, race: 'Ciamês' },
+    {id:2, name: 'Augusto', length: 0.5, weight: 2.0, race: 'Persa'},
+    {id:3, name: 'Ronaldo', length: 0.1, weight: 3.5, race: 'Burmê' },
+    {id:4, name: 'Gato', length: 0.2, weight: 6.0, race: 'Bengal' },
+    {id:5, name: 'Farofa', length: 0.8, weight: 1.0, race: 'Abissinio'},
+  ];
+
+
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
 
     if(this.route.routeConfig?.path?.includes("edit")) {
-      console.log("preencher formulario");
-      // preencher formulario
+      let id = this.route.snapshot.params['id'];
+      let cat = this.dataSource.find(item => item.id == id);
+      console.log(cat);
     }
 
-    console.log(this.route.snapshot.params)
+
 
   }
 }
