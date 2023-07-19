7aa96a8123ebb58fd32074f81c781e4550c2b871
Andre de Camargo <camargo_and@hotmail.com>
2023-07-19 18:03:11 -0300

    Configurando CatService

--- src/app/cats/cat.service.ts
+++ src/app/cats/cat.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Cat } from '../shared/models/Cat.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CatService {
 
-  constructor() { }
+  private serverUrl = 'api/cats';
+
+  constructor(private http: HttpClient) { }
+
+  getCats() {
+    return this.http.get<Cat[]>(this.serverUrl)
+  }
+
+  postCat() {}
+
+  putCat() {}
+
+
 }
